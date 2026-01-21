c851e4516758f97d8a5e6dbfd79092857af5e15b
KevinRay666 <18170460@itculiacan.edu.mx>
2026-01-21 16:16:21 -0700

    Se agrego el formulario y tabla donde se muestran los productos es funcional

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,48 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, FormsModule } from '@angular/forms';
+import { Producto } from './services/products/producto';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ReactiveFormsModule,
+    FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('prueba_front');
+export class App implements OnInit {
+
+  productoForm !: FormGroup;
+  producto: any[] = [];
+
+  constructor(
+    public fb: FormBuilder,
+    public productoService: Producto
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.productoForm = this.fb.group({
+      sku: ['', Validators.required],
+      nombre: ['', Validators.required],
+      precio: ['', Validators.required],
+      cantidad: ['', Validators.required],
+    })
+
+    this.productoService.getAllProductos().subscribe(resp => {
+      this.producto = resp.data;
+      console.log(resp.data);
+    },
+      error => { console.error(error) }
+    )
+
+  }
+
+  guardar() {
+
+  }
+
 }
